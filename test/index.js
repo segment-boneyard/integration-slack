f5832cdf8d916ef085f55cb0d2c58c1730f552b2
Aperçu <bgronon@gmail.com>
2016-04-16 14:59:22 +0200

    refactor(lint): switch to eslint

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
+'use strict';
 
 var Test = require('segmentio-integration-tester');
-var helpers = require('./helpers');
-var assert = require('assert');
 var Slack = require('..');
 
 describe('Slack', function() {
@@ -9,7 +8,7 @@ describe('Slack', function() {
   var settings;
   var test;
 
-  beforeEach(function(){
+  beforeEach(function() {
     settings = {
       webhookUrl: 'https://hooks.slack.com/services/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',
       channels: {},
@@ -19,7 +18,7 @@ describe('Slack', function() {
     test = Test(slack, __dirname);
   });
 
-  it('should have the correct settings', function(){
+  it('should have the correct settings', function() {
     test
       .name('Slack')
       .channels(['server', 'mobile', 'client'])
@@ -27,17 +26,17 @@ describe('Slack', function() {
   });
 
   describe('.validate()', function() {
-    it('should not be valid without a webhookUrl', function(){
+    it('should not be valid without a webhookUrl', function() {
       test.invalid({}, {});
     });
 
-    it('should be valid with a webhookUrl', function(){
+    it('should be valid with a webhookUrl', function() {
       test.valid({}, { webhookUrl: 'webhookUrl' });
     });
   });
 
   describe('.track()', function() {
-    it('should map track calls correctly', function(done){
+    it('should map track calls correctly', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
       output.username = 'Segment';
@@ -49,7 +48,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with email correctly', function(done){
+    it('should map track calls with email correctly', function(done) {
       var json = test.fixture('track-email');
       var output = json.output;
       output.username = 'Segment';
@@ -61,7 +60,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with name correctly', function(done){
+    it('should map track calls with name correctly', function(done) {
       var json = test.fixture('track-name');
       var output = json.output;
       output.username = 'Segment';
@@ -73,7 +72,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with first and last name correctly', function(done){
+    it('should map track calls with first and last name correctly', function(done) {
       var json = test.fixture('track-name-first-last');
       var output = json.output;
       output.username = 'Segment';
@@ -85,7 +84,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with username correctly', function(done){
+    it('should map track calls with username correctly', function(done) {
       var json = test.fixture('track-username');
       var output = json.output;
       output.username = 'Segment';
@@ -97,14 +96,14 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with custom channel correctly', function(done){
+    it('should map track calls with custom channel correctly', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
-      output.channel = '#testing-slack-api'
+      output.channel = '#testing-slack-api';
       output.username = 'Segment';
       output.icon_url = 'https://logo.clearbit.com/segment.com';
       settings.channels = {
-        "my-event": "#testing-slack-api"
+        'my-event': '#testing-slack-api'
       };
       test
         .set(settings)
@@ -113,13 +112,13 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with custom templates correctly', function(done){
+    it('should map track calls with custom templates correctly', function(done) {
       var json = test.fixture('track-template');
       var output = json.output;
       output.username = 'Segment';
       output.icon_url = 'https://logo.clearbit.com/segment.com';
       settings.templates = {
-        "Completed Order": "{{name}} bought an item worth ${{properties.revenue}}"
+        'Completed Order': '{{name}} bought an item worth ${{properties.revenue}}'
       };
       test
         .set(settings)
