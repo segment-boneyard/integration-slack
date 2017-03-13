62895fa13ad056d1bc233b01aad26bf0fac283f3
GNPMobile <patakygn@miamioh.edu>
2017-03-13 10:08:51 -0700

    Cleaned up the files to pass the linter as much as possible without removing anything (didn't change required imports)

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ describe('Slack', function() {
   var settings;
   var test;
 
-  beforeEach(function(){
+  beforeEach(function() {
     settings = {
       webhookUrl: 'https://hooks.slack.com/services/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',
       channels: {},
@@ -19,7 +19,7 @@ describe('Slack', function() {
     test = Test(slack, __dirname);
   });
 
-  it('should have the correct settings', function(){
+  it('should have the correct settings', function() {
     test
       .name('Slack')
       .channels(['server', 'mobile', 'client'])
@@ -27,17 +27,17 @@ describe('Slack', function() {
   });
 
   describe('.validate()', function() {
-    it('should not be valid without a webhookUrl', function(){
+    it('should not be valid without a webhookUrl', function() {
       test.invalid({}, {});
     });
 
-    it('should be valid with a webhookUrl', function(){
+    it('should be valid with a webhookUrl', function() {
       test.valid({}, { webhookUrl: 'webhookUrl' });
     });
   });
 
   describe('.track()', function() {
-    it('should map track calls correctly', function(done){
+    it('should map track calls correctly', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
       output.username = 'Segment';
@@ -49,13 +49,13 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should fail invalid templates gracefully', function(done){
+    it('should fail invalid templates gracefully', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
       output.username = 'Segment';
       output.icon_url = 'https://logo.clearbit.com/segment.com';
       settings.templates = {
-        "my-event": "{{invalid template"
+        'my-event': '{{invalid template'
       };
       test
         .set(settings)
@@ -63,7 +63,7 @@ describe('Slack', function() {
         .error(done);
     });
 
-    it('should map track calls with email correctly', function(done){
+    it('should map track calls with email correctly', function(done) {
       var json = test.fixture('track-email');
       var output = json.output;
       output.username = 'Segment';
@@ -75,7 +75,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with name correctly', function(done){
+    it('should map track calls with name correctly', function(done) {
       var json = test.fixture('track-name');
       var output = json.output;
       output.username = 'Segment';
@@ -87,7 +87,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with first and last name correctly', function(done){
+    it('should map track calls with first and last name correctly', function(done) {
       var json = test.fixture('track-name-first-last');
       var output = json.output;
       output.username = 'Segment';
@@ -99,7 +99,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with username correctly', function(done){
+    it('should map track calls with username correctly', function(done) {
       var json = test.fixture('track-username');
       var output = json.output;
       output.username = 'Segment';
@@ -111,14 +111,14 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with custom channel correctly', function(done){
+    it('should map track calls with custom channel correctly', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
-      output.channel = '#testing-slack-api'
+      output.channel = '#testing-slack-api';
       output.username = 'Segment';
       output.icon_url = 'https://logo.clearbit.com/segment.com';
       settings.channels = {
-        "my-event": "#testing-slack-api"
+        'my-event': '#testing-slack-api'
       };
       test
         .set(settings)
@@ -127,13 +127,13 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should map track calls with custom templates correctly', function(done){
+    it('should map track calls with custom templates correctly', function(done) {
       var json = test.fixture('track-template');
       var output = json.output;
       output.username = 'Segment';
       output.icon_url = 'https://logo.clearbit.com/segment.com';
       settings.templates = {
-        "Completed Order": "{{name}} bought an item worth ${{properties.revenue}}"
+        'Completed Order': '{{name}} bought an item worth ${{properties.revenue}}'
       };
       test
         .set(settings)
@@ -142,7 +142,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should decode HTML entities from templates', function(done){
+    it('should decode HTML entities from templates', function(done) {
       var json = test.fixture('track-entities');
       var output = json.output;
       settings.templates = {
@@ -157,7 +157,7 @@ describe('Slack', function() {
         .expects(200, done);
     });
 
-    it('should pass through an attachment to the Slack API call', function(done){
+    it('should pass through an attachment to the Slack API call', function(done) {
       var json = test.fixture('track-attachment');
       var output = json.output;
       output.username = 'Segment';
@@ -169,7 +169,7 @@ describe('Slack', function() {
           .expects(200, done);
     });
 
-    it('should pass through multiple attachments to the Slack API call', function(done){
+    it('should pass through multiple attachments to the Slack API call', function(done) {
       var json = test.fixture('track-attachment');
       var output = json.output;
       output.username = 'Segment';
